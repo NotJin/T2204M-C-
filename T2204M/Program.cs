1f060ecad05b163796e935bdc1eb866962cca579
Đồng Vũ <Dongvu7173@gmail.com>
2023-01-03 11:57:46 +0700

    up

--- T2204M/Program.cs
+++ T2204M/Program.cs
@@ -1,35 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-public class program
+﻿using T2204M.session1;
+public class Program
 {
-    public static void Main(String[] args)
+    public static void Main(string[] args)
     {
-        int x = 10;
-        double y = 5.14;
-        Console.WriteLine("Hello world: " + x);
+
+        double y = 3.14;
+
         string s = "T2204M";
 
+        int x = 10;
+        if (x >= 10)
+        {
+            Console.WriteLine("A");
+        }
+        else if (x > 5)
+        {
+            Console.WriteLine("B");
+        }
+        else if (x > 15)
+        {
+            Console.WriteLine("C");
+        }
+        else
+        {
+            Console.WriteLine("D");
+        }
+
         int[] ns = new int[3];
+
         ns[0] = 15;
         ns[1] = 20;
         ns[2] = 25;
 
-        foreach(int n in ns)
+        foreach (int n in ns)
         {
             //n
         }
 
-        for(int i = 0; i < ns.Length; i++)
+        for (int i = 0; i < ns.Length; i++)
         {
-            //ns[i]
+            // ns[i]
         }
 
-        string input = Console.ReadLine(); //scan
+        string input = Console.ReadLine(); // scan
         int num = Convert.ToInt32(input);
         Console.WriteLine("num = " + num);
 
         Human h = new Human();
-        h.name = "Nguyen Van A";
+        h.name = "Nguyen Van An";
         h.Run();
     }
 }
--- T2204M/Session1/Human.cs
+++ T2204M/Session1/Human.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace T2204M.Session1
+namespace T2204M.session1
 {
-    public  class Human
+    public class Human
     {
         public string name;
         public int age;
 
-    public void Eat
-    {
-        Console.WriteLine
+        public void Eat()
+        {
+            Console.WriteLine("Eating..");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(this.name + " running..");
+        }
     }
-}
-    
 }
